c57be275efa15ac1f2d33cf3936de8808c17f926
Adam La Morre <alamorre@gmail.com>
2022-07-16 00:52:59 -0400

    7 - User search logic added

--- src/ChatsPage/UserSearch.tsx
+++ src/ChatsPage/UserSearch.tsx
@@ -1,8 +1,12 @@
 import { AutoComplete, Input } from "antd";
 import type { SelectProps } from "antd/es/select";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-import { PersonObject } from "react-chat-engine-advanced";
+import { PersonObject, Avatar } from "react-chat-engine-advanced";
+
+import axios from "axios";
+
+import { privateKey, projectId } from "../hooks/constants";
 
 interface CustomChatFormProps {
   username: string;
@@ -11,12 +15,32 @@ interface CustomChatFormProps {
 }
 
 const UserSearch = (props: CustomChatFormProps) => {
+  const didMountRef = useRef(false);
+
+  const [loading, setLoading] = useState<boolean>(false);
   const [query, setQuery] = useState<string>("");
   const [users, setUsers] = useState<PersonObject[]>([]);
   const [options, setOptions] = useState<SelectProps<object>["options"]>([]);
 
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      const headers = { "Private-Key": privateKey };
+      axios
+        .get("https://api.chatengine.io/users/", { headers })
+        .then((r) => setUsers(r.data))
+        .catch();
+    }
+  });
+
   const searchResult = (query: string) => {
-    return users.map((user) => {
+    const foundUsers = users.filter(
+      (user) =>
+        JSON.stringify(user).toLowerCase().indexOf(query.toLowerCase()) !==
+          -1 && user.username !== props.username
+    );
+
+    return foundUsers.map((user) => {
       return {
         value: user.username,
         label: (
@@ -25,7 +49,17 @@ const UserSearch = (props: CustomChatFormProps) => {
               display: "flex",
               justifyContent: "space-between",
             }}
-          ></div>
+          >
+            <span>
+              <Avatar avatarUrl={user.avatar} username={user.username} />
+            </span>
+            <span>
+              <div>
+                {user.first_name} {user.last_name}
+              </div>
+              <div>{user.username}</div>
+            </span>
+          </div>
         ),
       };
     });
@@ -34,13 +68,34 @@ const UserSearch = (props: CustomChatFormProps) => {
     setOptions(query ? searchResult(query) : []);
   };
 
+  const onSelect = (value: string) => {
+    setLoading(true);
+
+    const headers = {
+      "Project-ID": projectId,
+      "User-Name": props.username,
+      "User-Secret": props.secret,
+    };
+    const data = {
+      usernames: [props.username, value],
+    };
+    axios
+      .put("https://api.chatengine.io/chats/", data, { headers })
+      .then((r) => {
+        props.onSelect(r.data.id);
+        setLoading(false);
+        setQuery("");
+      })
+      .catch(() => setLoading(false));
+  };
+
   return (
     <div>
       <AutoComplete
         dropdownMatchSelectWidth={252}
         className="ce-chat-form-autocomplete"
         options={options}
-        onSelect={(val: any) => console.log("val", val)}
+        onSelect={onSelect}
         onSearch={handleSearch}
         value={query}
       >
@@ -48,7 +103,8 @@ const UserSearch = (props: CustomChatFormProps) => {
           size="large"
           placeholder="Chats"
           enterButton
-          onChange={(e: any) => setQuery(e.target.value)}
+          loading={loading}
+          onChange={(e) => setQuery(e.target.value)}
         />
       </AutoComplete>
     </div>
